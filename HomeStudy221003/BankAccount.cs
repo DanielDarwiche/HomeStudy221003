ce9b9c1760f0cf6b353a5ac7e5c3a0c5f31f3ef5
DanielDarwiche <danielinti1@hotmail.com>
2022-10-04 09:07:08 +0200

    Add project files.

---
+++ HomeStudy221003/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeStudy221003
+{
+    public class BankAccount
+    {
+        public string Number { get; }//identieringsnummer
+        public string Owner { get; set; }//kontonamn
+        public decimal Balance 
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }//Saldo
+        private static int accountNumberSeed = 1234567890;//privat nr ! 
+        private List<Transaction> allTransactions = new List<Transaction>();
+        //lista av alla överföringar
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;    //this måste inte stå, funkar med/utan
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;//ändrar så att nästa konto har ett annat nr !
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive!");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive!");
+            } //THROW avbryter, forstätter inte koden i scoopet
+            if (Balance - amount < 0) 
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            //HEADER
+            report.AppendLine("Date  Amount Note");
+            foreach(var item in allTransactions)
+            {  //ROWS
+                report.AppendLine($"{item.Date.ToLongDateString()}   {item.Amount} {item.Notes}");
+
+            }
+            return report.ToString();
+        }
+
+    }
+}
---
+++ HomeStudy221003/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HomeStudy221003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 siffror identifiera bankkonto
+            //sträng för kontoägare
+            //saldo som kan ses
+            //kan sätta in pengar
+            //kan ta ut pengar
+            //startsaldo måste vara positivt
+            //uttag kan inte resultera i negativt saldo
+            var account = new BankAccount("Daniel", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "food");
+            Console.WriteLine(account.Balance);//köper mat,120 & visar saldo
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+        }
+    }
+}
